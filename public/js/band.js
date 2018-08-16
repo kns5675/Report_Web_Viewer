9a89e81488933379300e67b8841cd36dab55a7dd
landameizer <hyeongjoone@gmail.com>
2018-08-16 13:36:10 +0900

    커밋커밋

---
+++ public/js/band.js
@@ -0,0 +1,271 @@
+// 작성자 : 전형준
+// ==================== 밴드 배열 생성 메소드 ==================== //
+function CreateBandArray(data){
+    this.band_list = new Array();
+    if(Array.isArray(data.anyType)){ // Layer의 Band 또는 ChildBands 배열일 때
+        data.anyType.forEach(function(band){
+            this.band = CreateBand(band);
+            this.band_list.push(this.band);
+        });
+    } else{ // Layer의 Band 또는 ChildBands 배열이 아닐 때
+        this.band = CreateBand(data.anyType);
+        this.band_list.push(this.band);
+    }
+    return this.band_list;
+}
+// ============================================================= //
+
+
+
+// ========== 밴드 타입에 맞는 밴드 객체 생성을 위한 함수 ========== //
+function CreateBand(band){
+    var return_band = null;
+    switch(band._attributes["xsi:type"]){
+        case "BandBackGround" : // 백그라운드레이어 - 백그라운드밴드
+            return_band = new BandBackGround(band);
+            break;
+        case "BandForeGround" : // 포그라운드레이어 - 포그라운드밴드
+            return_band = new BandForeGround(band);
+            break;
+        case "BandPageHeader" : // 페이지 헤더 밴드
+            return_band = new BandPageHeader(band);
+            break;
+        case "BandTitle" : // 타이틀 밴드
+            return_band = new BandTitle(band);
+            break;
+        case "BandData" : // 데이터 밴드
+            return_band = new BandData(band);
+            break;
+        case "BandDataHeader" : // 데이터 헤더 밴드
+            return_band = new BandDataHeader(band);
+            break;
+        case "BandDataFooter" : // 데이터 풋터 밴드
+            return_band = new BandDataFooter(band);
+            break;
+        case "BandGroupHeader" : // 그룹 헤더 밴드
+            return_band = new BandGroupHeader(band);
+            break;
+        case "BandGroupFooter" : // 그룹 풋터 밴드
+            return_band = new BandGroupFooter(band);
+            break;
+        case "BandTail" : // 테일 밴드
+            return_band = new BandTail(band);
+            break;
+        case "BandPageFooter" : // 페이지 풋터 밴드
+            return_band = new BandPageFooter(band);
+            break;
+        case "BandSummary" : // 써머리 밴드
+            return_band = new BandSummary(band);
+            break;
+        case "BandDummy" : // 더미 밴드
+            return_band = new BandDummy(band);
+            break;
+        case "BandSubReport" : // 서브 리포트 밴드
+            return_band = new BandSubReport(band);
+            break;
+        case "BandDummyHeader" : // 더미 헤더 밴드
+            return_band = new BandDummyHeader(band);
+            break;
+        case "BandDummyFooter" : // 더미 풋터 밴드
+            return_band = new BandDummyFooter(band);
+            break;
+    }
+
+    return return_band;
+}
+// ============================================================== //
+
+
+// ======= 기본 밴드 객체 - 모든 밴드는 이 객체를 상속해서 사용(공통분모) ======= //
+function Band(data){
+    // this.parentId = data.ParentID;
+    // Bands에서 0번째 Band의 ParentID가 없음..
+
+    this.attributes = data._attributes; // 속성(밴드 타입)
+    this.id = data.Id._text; // ID
+    this.name = data.Name._text; // 이름
+    this.rectangle = { // 레이아웃(위치 + 크기)
+        x : (data.Rectangle.X === undefined ? "0" : data.Rectangle.X._text),
+        y : (data.Rectangle.Y === undefined ? "0" : data.Rectangle.Y._text),
+        width : data.Rectangle.Width._text,
+        height : (data.Rectangle.Height === undefined ? "0" : data.Rectangle.Height._text)
+    };
+
+    this.bandYOrder = data.BandYOrder._text;
+    // 아마 밴드 위치 설정하기 위한 옵션인 듯
+    // 디자이너에서만 사용되고 뷰어에는 불필요한 옵션인 듯
+
+    // this.childBands = data.ChildBands.anyType;
+    // this.childHeaderBands = data.ChildHeaderBands.anyType;
+    // this.childFooterBands = data.ChildFooterBands.anyType;
+
+    this.childBands // 자식 밴드들
+        = (typeof data.ChildBands.anyType === "undefined" ?
+        null : CreateBandArray(data.ChildBands));
+    this.childHeaderBands // 헤더의 자식 밴드들
+        = (typeof data.ChildHeaderBands.anyType === "undefined" ?
+        null : CreateBandArray(data.ChildHeaderBands));
+    this.childFooterBands // 풋터의 자식 밴드들
+        = (typeof data.ChildFooterBands.anyType === "undefined" ?
+        null : CreateBandArray(data.ChildFooterBands));
+    this.controlList // 임시로 해둔것 - 안예솔 팀원에게 물어보고 인자를 넘겨줄 것
+        = (typeof data.ControlList === undefined ? null : data.ControlList);
+}
+// ========================================================================== //
+
+
+// =============== Band 객체를 상속하여 만드는 각각의 Band 객체 =============== //
+// 백그라운드밴드
+function BandBackGround(band){
+    Band.apply(this, arguments);
+}
+
+// 포그라운드밴드
+function BandForeGround(band){
+    Band.apply(this, arguments);
+}
+
+// 페이지 헤더 밴드
+function BandPageHeader(band){
+    Band.apply(this, arguments);
+    this.pageOutputSkip = band.PageOutputSkip._text; // 첫 페이지 출력 생략
+    this.isApprovalBase = band.IsApprovalBase._text; // 결재란 기준 밴드
+    // 인쇄 미리보기 화면에서,
+    // 인쇄 고급 설정을 통하여 결재란을 그릴때 기준이 된 밴드에 결재란을 그린다
+}
+
+// 페이지 풋터 밴드
+function BandPageFooter(band){
+    Band.apply(this, arguments);
+    this.pageOutputSkip = band.PageOutputSkip._text; // 먹통 - 끝페이지 출력 생략
+}
+
+// 써머리 밴드
+function BandSummary(band){
+    Band.apply(this, arguments);
+    this.ignoreNewPage = band.IgnoreNewpage._text; // 먹통 - 새 페이지 무시
+    this.topToDownAlign = band.TopToDownAlign._text; // 먹통 - 위에서 아래로 정렬
+    this.isBottom = band.IsBottom._text; // 페이지 하단에 출력
+}
+
+// 데이터 헤더 밴드
+function BandDataHeader(band){
+    Band.apply(this, arguments);
+    this.showGroupLink = band.ShowGroupLink._text; // 그룹 연결 보기
+    this.forceNewPage = band.ForceNewPage._text; // 페이지 넘기기
+    this.autoSize = band.AutoSize._text; // 자동 높이 조정
+    this.isApprovalBase = band.IsApprovalBase._text; // 결재란 기준 밴드
+    // 인쇄 미리보기 화면에서,
+    // 인쇄 고급 설정을 통하여 결재란을 그릴때 기준이 된 밴드에 결재란을 그린다
+}
+
+// 데이터 풋터 밴드
+function BandDataFooter(band){
+    Band.apply(this, arguments);
+    this.showGroupLink = band.ShowGroupLink._text; // 그룹 연결 보기
+    this.forceNewPage = band.ForceNewPage._text; // 페이지 넘기기
+    this.autoSize = band.AutoSize._text; // 자동 높이 조정
+}
+
+// 데이터 밴드
+function BandData(band){
+    Band.apply(this, arguments);
+    this.hasHeader = band.HasHeader._text; // 헤더밴드 소유
+    this.hasFooter = band.HasFooter._text; // 풋터밴드 소유
+    this.showGroupLink = band.ShowGroupLink._text; // 그룹 연결 보기
+    this.dummyHeaderCount = band.DummyHeaderCount._text; // 더미(헤더)
+    this.dummyFooterCount = band.DummyFooterCount._text; // 더미(풋터)
+    this.subBandCount = band.SubBandCount._text; // 서브 밴드
+    this.fixMaster = band.FixMaster._text; // 마스터 고정
+    this.fixTitle = band.FixTitle._text; // 데이터 헤더 밴드 고정
+    this.forceNewPage = band.ForceNewPage._text; // 페이지 넘기기
+    this.invisible = band.Invisible._text; // 감추기
+    this.fixPriorGroupHeader = band.FixPriorGroupHeader._text; // 상위 그룹 헤더 고정
+    this.fixPriorGroupFooter = band.FixPriorGroupFooter._text; // 상위 그룹 풋터 고정
+    this.autoSize = band.AutoSize._text; // 자동 높이 조정
+    this.repeatNumber = band.RepeatNumber._text; // 반복 횟수
+    this.dziName = band.DziName._text; // 데이터 셋 이름
+    this.dataTableName = band.DataTableName._text; // 테이블 이름
+    this.isContainTypeDynamicTable = band.IsContainTypeDynamicTable._text; // 동적테이블 보유 여부
+    this.isContainTypeChart = band.IsContainTypeChart._text; // 차트 보유 여부
+    this.isContainTypeCrossTable = band.IsContainTypeCrossTable._text; // 크로스탭 테이블 보유 여부
+    this.isContainTypeRegion = band.IsContainTypeRegion._text; // 리전 보유 여부
+}
+
+// 그룹 헤더 밴드
+function BandGroupHeader(band){
+    Band.apply(this, arguments);
+    this.showGroupLink = band.ShowGroupLink._text; // 그룹 연결 보기
+    this.subBand = band.SubBand._text; //
+    this.forceNewPage = band.ForceNewPage._text; // 페이지 넘기기
+    this.fixPriorGroupHeader = band.FixPriorGroupHeader._text; // 상위 그룹 헤더 고정
+    this.invisible = band.Invisible._text; // 감추기
+    this.groupingFieldSortOption = band.GroupingFieldSortOption._text;
+    this.groupingFieldSort = band.GroupingFieldSort._text;
+    this.autoSize = band.AutoSize._text; // 자동 높이 조정
+    this.dziName = band.DziName._text; // 데이터 셋 이름
+    this.dataTableName = band.DataTableName._text; // 데이터 테이블 이름
+    this.groupFiledName = band.GroupFiledName._text; // 그룹 기준 필드
+    this.isApprovalBase = band.IsApprovalBase._text; // 결재란 기준 밴드
+}
+
+// 그룹 풋터 밴드
+function BandGroupFooter(band){
+    Band.apply(this, arguments);
+    this.subBand = band.SubBand._text; // 서브 밴드
+    this.forceNextReport = band.ForceNextReport._text; // 리포트 넘기기
+    this.forceNewPage = band.ForceNewPage._text; // 페이지 넘기기
+    this.repeatNumber = band.RepeatNumber._text; // 반복 횟수
+    this.invisible = band.Invisible._text; // 감추기
+    this.autoSize = band.AutoSize._text; // 자동 높이 조정
+    this.dziName = band.DziName._text; // 데이터 셋 이름
+    this.dataTableName = band.DataTableName._text; // 데이터 테이블 이름
+    this.groupFiledName = band.GroupFiledName._text; // 그룹 기준 필드
+}
+
+// 타이틀 밴드
+function BandTitle(band){
+    Band.apply(this, arguments);
+    this.dummyHeaderCount = band.DummyHeaderCount._text; // 더미(헤더)
+    this.dummyFooterCount = band.DummyFooterCount._text; // 더미(풋터)
+    this.forceNewPage = band.ForceNewPage._text; // 먹통?? - 페이지 넘기기
+    this.autoSize = band.AutoSize._text; // DRD에서 수정 불가 속성 - 먹통?
+    this.isApprovalBase = band.IsApprovalBase._text; // 결재란 기준 밴드
+    // 인쇄 미리보기 화면에서,
+    // 인쇄 고급 설정을 통하여 결재란을 그릴때 기준이 된 밴드에 결재란을 그린다
+}
+
+// 테일 밴드
+function BandTail(band){
+    Band.apply(this, arguments);
+    this.invisible = band.Invisible._text; // 감추기
+}
+
+// 더미 밴드
+function BandDummy(band){
+    Band.apply(this, arguments);
+    this.forceNewPage = band.ForceNewPage._text; // 먹통?? - 페이지 넘기기
+    this.stopAtPageEnd = band.StopAtPageEnd._text; // 값의 의미가 없는 속성?
+    this.repeatNubmer = band.RepeatNubmer._text; // DRD에서 수정 불가 속성 - 먹통?
+    this.autoSize = band.AutoSize._text; // DRD에서 수정 불가 속성 - 먹통?
+}
+
+// 서브 리포트 밴드
+function BandSubReport(band){
+    Band.apply(this, arguments);
+    this.showGroupLink = band.ShowGroupLink._text; // 먹통?? - 그룹 연결 보기
+}
+
+// 더미 헤더 밴드
+function BandDummyHeader(band){
+    Band.apply(this, arguments);
+    this.forceNewPage = band.ForceNewPage._text; // 페이지 넘기기
+}
+
+// 더미 풋터 밴드
+function BandDummyFooter(band){
+    Band.apply(this, arguments);
+    this.forceNewPage = band.ForceNewPage._text; // 페이지 넘기기
+    this.useSpaceEffect = band.UseSpaceEffect._text; // 여백 효과 사용
+}
+// ========================================================================== //
---
+++ public/js/report_layer.js
@@ -0,0 +1,108 @@
+// 작성자 : 전형준
+// ========================== ReportTemplate 객체 ========================== //
+function ReportTemplate(data){
+    this.attributes = data.ReportTemplate._attributes;
+    // reportTemplate.attributes["xmlns:xsd"];
+    // reportTemplate.attributes["xmlns:xsi"]; // 이런식으로 접근할 것
+    this.id = data.ReportTemplate.Id._text; // ID
+    this.rectangle = data.ReportTemplate.Rectangle._text; // ReportTemplate에게 의미 없는 속성
+    this.name = data.ReportTemplate.Name._text; // 이름
+    this.printOder = data.ReportTemplate.PrintOder._text; // 인쇄 순서
+    this.savedLocation = data.ReportTemplate.SavedLocation._text; // 파일 저장 경로
+    this.mesureUnit = data.ReportTemplate.MesureUnit._text; // 사용 단위계
+    this.isUsePrinterHardMargin = data.ReportTemplate.IsUsePrinterHardMargin._text; //프린터 기본여백 사용
+    this.gridGap = { // 그리드 간격
+        width : data.ReportTemplate.GridGap.Width._text,
+        height : data.ReportTemplate.GridGap.Height._text
+    }
+
+    this.drawGrid = data.ReportTemplate.DrawGrid._text; // 그리드 라인 표시
+    this.snapToGrid = data.ReportTemplate.SnapToGrid._text; // 그리드 눈금에 따른 이동
+
+    // 웹버전은 가상화 버전과 관련이 없으므로 해당 속성에 대해서는 무시하여도 무관 //
+    this.isVirtureToLocalPrint = data.ReportTemplate.IsVirtureToLocalPrint._text;
+    this.imageListVirtual = data.ReportTemplate.ImageListVirtual._text;
+    // 웹버전은 가상화 버전과 관련이 없으므로 해당 속성에 대해서는 무시하여도 무관 //
+
+    this.reportList = (function(data){ // ReportList 배열 생성(Report가 한개든 이상이든)
+        this.list = new Array();
+
+        // anyType이 배열인지 체크 (List에 Report가 1개만 존재)
+        if(!Array.isArray(data.anyType)){
+            this.report = new Report(data.anyType);
+            this.list.push(this.report);
+        } else{ // List에 Report가 2개 이상 존재
+            data.anyType.forEach(function(report){
+                this.report = new Report(report);
+                this.list.push(this.report);
+            });
+        }
+        return list;
+    })(data.ReportTemplate.ReportList);
+
+    this.isLinkedReport = data.ReportTemplate.IsLinkedReport._text; // 리포트 연결 인쇄
+    this.pasteMode = data.ReportTemplate.PasteMode._text; // 확대축소 붙이기 모드
+    this.isUseExportPerGroup = data.ReportTemplate.IsUseExportPerGroup._text; // 그룹별 엑셀 내보내기 사용
+}
+// ========================================================================== //
+
+
+// =============================== Report 객체 ============================== //
+function Report(data){
+    // 왜인지 모르겠지만 Sample01.DRF의 3번째 Report에서 ParentID가 존재하지 않음, 객체에 넣지 않았음
+    // this.parentId = data.ParentID._text;
+    this.id = data.Id._text; // id
+    this.rectangle = { // 크기
+        width : data.Rectangle.Width._text,
+        height : data.Rectangle.Height._text
+    }
+    this.name = data.Name._text; // 이름
+    this.lock = data.Lock._text; // 잠김
+
+    // 인쇄 무시하기란 다중의 리포트가 있는 경우 특정 리포트에 IsIgnore값을
+    // true로 하게 되면 데이터 유무와 관계없이 해당 리포트는 인쇄 미리보기에서 제외함
+    this.isIgnore = data.IsIgnore._text; // 인쇄 무시하기
+    this.forceNewPage = data.ForceNewPage._text; // 페이지 넘기기
+    this.forceNextReport = data.ForceNextReport._text; // 리포트 넘기기
+    this.multyFormCount = data.MultyFormCount._text; // 다단 전표 수
+    this.annexPaper = data.AnnexPaper._text; // 별지(서브리포트)
+    this.noPrintIfNoData = data.NoPrintIfNoData._text; // 데이터 테이블에 따른 인쇄 유무
+
+    // Layers
+    this.layers = {
+        backGroundLayer : new Layer(data.Layers.anyType[0]),
+        designLayer : new Layer(data.Layers.anyType[1]),
+        foreGroundLayer : new Layer(data.Layers.anyType[2])
+    };
+
+    this.margin = { // 여백
+        x : data.Margin.X._text,
+        y : data.Margin.Y._text,
+        width : data.Margin.Width._text,
+        height : data.Margin.Height._text
+    };
+    this.paperDirection = data.PaperDirection._text; // 용지 방향
+    this.gutter = data.Gutter._text; // 제본용 여백
+    this.gutterPosition = data.GutterPosition._text; // 제본용 여백 위치
+    this.morrirMargins = data.MorrirMargins._text; // 페이지 마주보기
+    this.paperType = data.PaperType._text; // 용지 타입
+    this.paperSize = { // 페이지 크기
+        width : data.PaperSize.Width._text,
+        height : data.PaperSize.Height._text
+    };
+    this.showPageBorder = data.ShowPageBorder;
+    this.hideVirtualArea = data.HideVirtualArea; // 가상 영역 숨김
+    this.standardMarginPixel = data.StandardMarginPixel; // 라벨 기본 여백값
+}
+// ========================================================================== //
+
+
+
+// =============== Layer 객체(ForeGround, Design, BackGround) =============== //
+function Layer(data){
+    this.attributes = data._attributes;
+    this.name = data.Name._text;
+    this.isActive = data.IsActive._text;
+    this.bands = CreateBandArray(data.Bands); // 각 Layer가 갖고 있는 Bands
+}
+// ========================================================================== //
