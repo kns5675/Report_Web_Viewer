3130ad41e0192e6ea36c59497b9d404d6b164fcd
powerku <powerku@naver.com>
2018-08-16 16:38:13 +0900

    Merge branch 'master' of https://github.com/kns5675/Report_Web_Viewer
    # Conflicts:
    #	node_modules/express/node_modules/body-parser/package.json
    #	node_modules/express/node_modules/iconv-lite/package.json
    #	node_modules/express/node_modules/raw-body/node_modules/depd/package.json
    #	node_modules/express/node_modules/raw-body/node_modules/http-errors/package.json
    #	node_modules/express/node_modules/raw-body/node_modules/setprototypeof/package.json
    #	node_modules/express/node_modules/raw-body/package.json
    #	node_modules/xml-parser/package.json

--- public/js/report.js
+++ public/js/report.js
@@ -1,5 +1,10 @@
 var pageNum = 1;
+var reportNum = 1;
 
+/******************************************************************
+ 기능 : 페이지에서 Json 파일을 매개변수로 받아서 ReportTemplate를 만듬
+ author : powerku
+ ******************************************************************/
 function makeReportTemplate(data) {
     var reportTemplate = new ReportTemplate(data);
 
@@ -11,38 +16,82 @@ function makeReportTemplate(data) {
 }
 
 /******************************************************************
- 기능 : 페이지를 만든다.
- 만든이 : 구영준
+ 기능 : make report in function makeReportTemplate
+ author : powerku
  ******************************************************************/
-function makeReport(report) {
+
+ function makeReport(report) {
 
     console.log(report);
-    setPage(report.paperSize.width, report.paperSize.height);
+
+    setPage(report);
     setReport(report);
+
     pageNum++
 }
-function setReport(report){
-    $(('#page' + pageNum)).append('<div id="report' + pageNum + '"></div>');
 
-    $('#report' + pageNum).css('width', report.rectangle.width);
-    $('#report' + pageNum).css('height', report.rectangle.height);
-    $('#report' + pageNum).css('background-color', 'blue');
-    $('#report' + pageNum).css('border', 'black');
-    $('#report' + pageNum).css('margin-top', report.margin.x+'px');
-    $('#report' + pageNum).css('margin-bottom', report.margin.y+'px');
-    $('#report' + pageNum).css('margin-left', report.margin.height+'px');
-    $('#report' + pageNum).css('margin-right', report.margin.width+'px');
+/******************************************************************
+ 기능 : 페이지안의 리포트를 만듬
+ author : powerku
+ ******************************************************************/
+function setReport(report){
+    $(('#page' + pageNum)).append('<div id="report' + reportNum + '"class = report' + reportNum + '></div>');
+
+
+    setReportDirection(report);
+    $('#report' + reportNum).css('background-color', 'blue');
+    $('#report' + reportNum).css('border', 'black');
+    $('#report' + reportNum).css('margin-top', report.margin.x+'px');
+    $('#report' + reportNum).css('margin-bottom', report.margin.y+'px');
+    $('#report' + reportNum).css('margin-left', report.margin.height+'px');
+    $('#report' + reportNum).css('margin-right', report.margin.width+'px');
+
+    reportNum++;
+}
+
+/******************************************************************
+ 기능 : 페이지안의 리포트 방향 설정
+ author : powerku
+ ******************************************************************/
+function setReportDirection(report){
+
+    if(report.paperDirection){
+        $('#report' + reportNum).css('width', report.rectangle.width);
+        $('#report' + reportNum).css('height', report.rectangle.height);
+    }else{
+        $('#report' + reportNum).css('height', report.rectangle.width);
+        $('#report' + reportNum).css('width', report.rectangle.height);
+
+    }
 
 }
 
-
-function setPage(width, height) {
+/******************************************************************
+ 기능 : 페이지 크기 렌더링
+ author : powerku
+ ******************************************************************/
+function setPage(report) {
     $('#reportTemplate').append('<div id="page' + pageNum + '" class="page"></div>');
 
-    $('#page' + pageNum).css('width', width);
-    $('#page' + pageNum).css('height', height);
+    setPageDirection(report);
     $('#page' + pageNum).css('background-color', 'yellow');
     $('#page' + pageNum).css('border', 'solid blue');
 
+
+
+}
+
+/******************************************************************
+ 기능 : 페이지 방향 설정
+ author : powerku
+ ******************************************************************/
+function setPageDirection(report){
+    if(report.paperDirection){
+        $('#page' + pageNum).css('width', report.paperSize.width);
+        $('#page' + pageNum).css('height', report.paperSize.height);
+    }else{
+        $('#page' + pageNum).css('height', report.paperSize.width);
+        $('#page' + pageNum).css('width', report.paperSize.height);
+    }
 }
 
