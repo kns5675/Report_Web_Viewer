17183c6254994266480160f4bfecd3cb5490b5b2
811039ha <811039@gmail.com>
2018-08-17 16:21:40 +0900

    커밋

--- public/js/viewer.js
+++ public/js/viewer.js
@@ -1,3 +1,7 @@
+/******************************************************************
+ 기능 : DR Viewer 인쇄 메뉴 선택하기 (select option value받아와서 메뉴 인식 후 모달창 띄우기)
+ author : 하지연
+ ******************************************************************/
 function copyoptions(){
     try{
         if ($("#copyOptions").val() == 'ecopy')
@@ -14,11 +18,20 @@ function copyoptions(){
         console.log(e.message);
     }
 }
+/******************************************************************
+ 기능 : DR Viewer의 zoomIn,zoomOut기능에서 확대 또는 축소의 값변경 판단
+ author : 하지연
+ ******************************************************************/
 function sizechanged(){
     $("#txtZoom").bind("change", function(){
         test();
     });
 }
+/******************************************************************
+ 기능 : DR Viewer의 zoomIn,zoomOut기능에서 확대 또는 축소 변경 값을 받아온 후,
+        다양한 브라우저의 scale값을 조정하여 zoomIn, zoomOut기능 구현
+ author : 하지연
+ ******************************************************************/
 function test(){
     try {
         var flexiblecontent = document.getElementById("reportTemplate");
@@ -40,6 +53,11 @@ function test(){
         console.log(e.message);
     }
 }
+/******************************************************************
+ 기능 : select option의 선택값을 기본으로 +버튼 클릭 시 5%가 추가 확대되는
+        값을 받아와 zoomIn기능 구현
+ author : 하지연
+ ******************************************************************/
 function zoomIn(){
     try {
         var size = $("#txtZoom").val();
@@ -59,6 +77,11 @@ function zoomIn(){
         alert("test ==> " + e.message);
     }
 }
+/******************************************************************
+ 기능 : select option의 선택값을 기본으로 -버튼 클릭 시 5%가 추가 축소되는
+        값을 받아와 zoomOut기능 구현
+ author : 하지연
+ ******************************************************************/
 function zoomOut(){
     try {
         var size = $("#txtZoom").val();
@@ -80,6 +103,10 @@ function zoomOut(){
         alert(e.message);
     }
 }
+/******************************************************************
+ 기능 : 모달창의 닫기 버튼
+ author : 하지연
+ ******************************************************************/
 function close_pop() {
     $('#myModal').hide();
 };
