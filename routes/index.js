951235ff5f71bba16d172b7bf6095399784a2ce3
811039ha <811039ha@gmail.com>
2018-09-05 10:38:50 +0900

    이미지 내보내기 완성, 더미코드 삭제

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var router = express.Router();
 var convert = require('xml-js');
 var fs = require('fs');
 var xml = fs.readFileSync('xml/TestSample.xml', 'utf-8');
+//var xml = fs.readFileSync('xml/Sample01.xml', 'utf-8');
 // var inspect = require('util').inspect;
 var json_origin = convert.xml2json(xml, {compact : true});
 var json = json_origin.replace(/\\r/gi, '<br/>'); // 엔터키(\r)를 <br/>로 치환
 
 var tempData = fs.readFileSync('xml/db_TestSample.xml', 'utf-8');
-// var tempData = fs.readFileSync('xml/TestSample.xml', 'utf-8');
 var dataTable = convert.xml2json(tempData, {compact : true});
 
 /* GET home page. */
