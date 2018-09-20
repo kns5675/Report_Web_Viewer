436ca796c16dfc486cc91da7e1e4710685b3c173
구영준 <powerku@naver.com>
2018-09-20 18:14:14 +0900

    BandData와 DataTable이 맞지 않을 때, 빈 테이블이 나오도록 수정

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 var convert = require('xml-js');
 var fs = require('fs');
-var xml = fs.readFileSync('xml/Region.xml', 'utf-8');
+var xml = fs.readFileSync('xml/GroupParameter.xml', 'utf-8');
 
 var json_origin = convert.xml2json(xml, {compact: true});
 var json = json_origin.replace(/\\r/gi, '<br/>'); // 엔터키(\r)를 <br/>로 치환
 
-var tempData = fs.readFileSync('xml/Region_Data.xml', 'utf-8');
+var tempData = fs.readFileSync('xml/GroupParameterData.xml', 'utf-8');
 var dataTable = convert.xml2json(tempData, {compact: true});
 
 var paramData = fs.readFileSync('xml/GroupParameterData_Param.xml', 'utf-8');
