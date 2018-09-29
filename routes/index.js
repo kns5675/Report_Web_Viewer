c3dcdefca0129e6a068e0863f7f0b2b6a6c57074
kns5675 <wnsl5675@naver.com>
2018-09-29 16:20:08 +0900

    파일 열기 모달창 추가 및 css 작업 완료 파일 열기 기능 완료(예외처리 안된 부분 있음) shift+ 클릭(드래그)를 통해서 해당 컬럼 크기 조정을 하면 그에 맞춰 테이블도 함께 사이즈 조정되는 기능 추가 작업 중. 이미지 추가 중에 파일 선택을 안하고 취소를 누를 경우 깨진 파일이 추가되는 버그 수정. 파일 저장 기능 추가 작업 중. 동적 테이블 라벨 text를 p태그로 boxing하여 text변경시 크기변경, 위치이동 기능이 사라지는 버그 수정 완료.

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,31 @@ var router = express.Router();
 
 var convert = require('xml-js');
 var fs = require('fs');
-var xml = fs.readFileSync('xml/GroupParameter.xml', 'utf-8');
 
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/') // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname) // cb 콜백함수를 통해 전송된 파일 이름 설정
+    }
+});
+var upload = multer({ storage: storage });
+
+var xml = fs.readFileSync('xml/TestSample.xml', 'utf-8');
 var json_origin = convert.xml2json(xml, {compact: true});
 var json = json_origin.replace(/\\r/gi, '<br/>'); // 엔터키(\r)를 <br/>로 치환
+var test = convert.json2xml(json, {compact: true});
+// var overlap =
+// var temp_file_name = "message"+overlap+".xml";
+// fs.writeFile('xml/'+, test, (err) => {
+//     if (err) throw err;
+//     console.log('The file has been saved!');
+// });
 
-var tempData = fs.readFileSync('xml/GroupParameterData.xml', 'utf-8');
+var tempData = fs.readFileSync('xml/db_TestSample.xml', 'utf-8');
+// var tempData = fs.readFileSync('xml/GroupParameterData.xml', 'utf-8');
 var dataTable = convert.xml2json(tempData, {compact: true});
 
 var paramData = fs.readFileSync('xml/GroupParameterData_Param.xml', 'utf-8');
@@ -21,8 +40,37 @@ router.get('/', function (req, res) {
         layout: false,
         data: json,
         dataTable: dataTable,
-        paramTable: paramTable
+        paramTable: paramTable,
+        // subReport: dataTable2,
+        // dataTable2: dataTable3
     });
+});
+
+router.post('/', upload.array('send_file', 3), function (req, res, next) {
+    // res.send('Uploaded! : '+req.file); // object를 리턴함
+    // console.log(req.files[0].filename);
+    var file_name = 'uploads/'+req.files[0].filename;
+    var db_name = 'uploads/'+req.files[1].filename;
+    var param_name = 'uploads/'+req.files[2].filename;
+
+    var xml = fs.readFileSync(file_name, 'utf-8');
+    var json_origin = convert.xml2json(xml, {compact: true});
+    var json = json_origin.replace(/\\r/gi, '<br/>');
+
+    var tempData = fs.readFileSync(db_name, 'utf-8');
+    var dataTable = convert.xml2json(tempData, {compact: true});
+
+    var paramData = fs.readFileSync(param_name, 'utf-8');
+    var paramTable = convert.xml2json(paramData, {compact: true});
+
+    if(req.files[0]){
+        res.render('index', {
+            layout: false,
+            data: json,
+            dataTable: dataTable,
+            paramTable: paramTable
+        });
+    }
 });
 
 module.exports = router;
